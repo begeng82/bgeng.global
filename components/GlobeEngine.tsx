81b373be52009942091ff9288508bb965a9d2041
begeng82 <poyanbegeng@gmail.com>
2026-03-08 04:26:20 +0700

    Update GlobeEngine.tsx

--- components/GlobeEngine.tsx
+++ components/GlobeEngine.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls, Stars, Html, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
@@ -7,21 +7,23 @@ import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-export default function GlobeEngine() {
+function GlobeContent() {
   const globeRef = useRef<any>(null);
   const cloudRef = useRef<any>(null);
 
-  // DATA GEMPA REAL-TIME (Update tiap 5 detik)
+  // Data Gempa Lifetime (Update tiap 5 detik)
   const { data } = useSWR(
     "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson",
     fetcher, { refreshInterval: 5000 }
   );
 
-  const [map, night, clouds, normal] = useLoader(THREE.TextureLoader, [
+  // Load Tekstur HD
+  const [map, night, clouds, normal, spec] = useLoader(THREE.TextureLoader, [
     'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_atmos_2048.jpg',
     'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_lights_2048.png',
     'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_clouds_1024.png',
-    'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_normal_2048.jpg'
+    'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_normal_2048.jpg',
+    'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_specular_2048.jpg'
   ]);
 
   useFrame(({ clock }) => {
@@ -35,8 +37,8 @@ export default function GlobeEngine() {
       <PerspectiveCamera makeDefault position={[0, 0, 8]} />
       <OrbitControls enableDamping minDistance={2.5} maxDistance={15} />
       <Stars radius={300} count={15000} factor={7} fade />
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} intensity={3} color="#00f2ff" />
+      <ambientLight intensity={0.8} />
+      <pointLight position={[10, 10, 10]} intensity={2.5} color="#00f2ff" />
 
       <group ref={globeRef}>
         <mesh>
@@ -44,26 +46,23 @@ export default function GlobeEngine() {
           <meshStandardMaterial 
             map={map} 
             normalMap={normal}
+            roughnessMap={spec}
             emissiveMap={night} 
             emissive={new THREE.Color("#ffd27d")} 
-            emissiveIntensity={1.5}
+            emissiveIntensity={1.8}
           />
         </mesh>
-        
-        {/* Layer Awan */}
         <mesh ref={cloudRef} scale={1.02}>
           <sphereGeometry args={[2, 128, 128]} />
           <meshStandardMaterial map={clouds} transparent opacity={0.4} blending={THREE.AdditiveBlending} />
         </mesh>
-
-        {/* Glow Atmosfer */}
+        {/* Atmosphere Outer Glow */}
         <mesh scale={1.15}>
           <sphereGeometry args={[2, 128, 128]} />
-          <meshBasicMaterial color="#00f2ff" transparent opacity={0.08} side={THREE.BackSide} />
+          <meshBasicMaterial color="#00f2ff" transparent opacity={0.1} side={THREE.BackSide} />
         </mesh>
 
-        {/* Marker Gempa */}
-        {data?.features?.slice(0, 30).map((f: any, i: number) => {
+        {data?.features?.slice(0, 40).map((f: any, i: number) => {
           const [lng, lat] = f.geometry.coordinates;
           const phi = (90 - lat) * (Math.PI / 180);
           const theta = (lng + 180) * (Math.PI / 180);
@@ -84,4 +83,12 @@ export default function GlobeEngine() {
       </group>
     </>
   );
+}
+
+// WRAPPER UNTUK CEK CLIENT SIDE
+export default function GlobeEngine() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => { setMounted(true); }, []);
+  if (!mounted) return null;
+  return <GlobeContent />;
 }

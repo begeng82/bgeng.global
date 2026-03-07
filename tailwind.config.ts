c946d5589c7463805c174070e36512a73fe0c0e5
begeng82 <poyanbegeng@gmail.com>
2026-03-08 04:16:21 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,14 +9,7 @@ const config: Config = {
     extend: {
       animation: {
         'spin-slow': 'spin 20s linear infinite',
-        'marquee': 'marquee 25s linear infinite',
       },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-50%)' },
-        }
-      }
     },
   },
   plugins: [],

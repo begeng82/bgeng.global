d281bfd6b1ae7dc1e9c9d4b650f0850f62ee8e60
begeng82 <poyanbegeng@gmail.com>
2026-03-08 04:47:15 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'OMNIS V20 | TACTICAL COMMAND',
+  title: 'OMNIS V20 | GOD TIER',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-black antialiased">{children}</body>
+      <body className="antialiased crt">{children}</body>
     </html>
   )
 }

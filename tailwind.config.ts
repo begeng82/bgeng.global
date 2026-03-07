04546f46c62c536678b19777b7da3664e9ea8fbf
begeng82 <poyanbegeng@gmail.com>
2026-03-08 04:21:04 +0700

    tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,14 @@ const config: Config = {
     extend: {
       animation: {
         'spin-slow': 'spin 20s linear infinite',
+        'marquee': 'marquee 30s linear infinite',
       },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
+      }
     },
   },
   plugins: [],
